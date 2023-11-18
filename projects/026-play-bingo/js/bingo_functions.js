df12d53fe704e4ec6f44ba9e59f2d692d86c3f95
rosario <rosariospalla.job@gmail.com>
2023-11-18 17:51:58 +0100

    feat: create a program that simulates a lot of bingo games with one player and also show statistics about the games

---
+++ projects/026-play-bingo/js/bingo_functions.js
@@ -0,0 +1,174 @@
+function createBingoCard() {
+    const bingoCard = {};
+
+    const bingoColumns = ["B", "I", "N", "G", "O"];
+
+    const max = 15;
+    let min = 1;
+
+    bingoColumns.forEach( bingoColumn => {
+        const bingoValues = [];
+
+        for (let i = 0; i < 5; i++) {
+            const randomNumber = Math.floor(Math.random() * max + min);
+
+            if (!bingoValues.includes(randomNumber)) {
+                bingoValues.push(randomNumber);
+                continue;
+            }
+
+            i--;
+
+        }
+
+        bingoCard[bingoColumn] = bingoValues;
+
+        min += 15;
+    });
+
+    return bingoCard;
+}
+
+function displayBingoCard(bingoCard) {
+    let bingoLabes = "";
+
+    for (const key in bingoCard) {
+        bingoLabes += key + "  | ";
+    }
+
+    let bingoRows = "";
+
+    for (let i = 0; i < 5; i++) {
+
+        for (const key in bingoCard) {
+            bingoRows += bingoCard[key][i].toString().padStart(2, "0") + " | ";
+        }
+
+        bingoRows += "\n";
+    }
+
+    return console.log( `${bingoLabes}\n${bingoRows}` );
+}
+
+function markBingoCard(bingoCard, extractNumber) {
+
+    for (const key in bingoCard) {
+            
+        const index = bingoCard[key].indexOf(extractNumber);
+        if (index !== -1) {
+            bingoCard[key][index] = 0;
+        }
+    }
+    
+}
+
+function checkWinningBingoCard(bingoCard) {
+    
+    // Case 1 - All numbers in the column are marked
+    for (const key in bingoCard) {
+
+        const winningColumn = bingoCard[key].every( number => {
+            return number === 0;
+        })
+
+        if (winningColumn) {
+            // console.log("Column win");
+            return true;
+        }
+
+    }
+
+    // Case 2 - All numbers in a row are marked
+    for (let i = 0; i < 5; i++) {
+        let winningRow = 0;
+
+        for (const key in bingoCard) {
+
+            if (bingoCard[key][i] === 0) {
+                winningRow++;
+            }
+        }
+
+        if (winningRow === 5) {
+            // console.log("Row win");
+            return true
+        }
+    }
+
+    // Case 3 - Diagonal are marked
+    if (bingoCard["N"][2] === 0) {
+
+        if ( (bingoCard["G"][1] === 0 && bingoCard["I"][3] === 0) && (bingoCard["B"][4] === 0 && bingoCard["O"][0] === 0) ) {
+            
+            // console.log("Diagonal win");
+            return true;
+        }
+        else if ( (bingoCard["B"][0] === 0 && bingoCard["O"][4] === 0) && (bingoCard["G"][3] === 0 && bingoCard["I"][1] === 0) ) {
+
+            // console.log("Diagonal");
+            return true;
+        }
+    }
+
+    return false;
+}
+
+// // This part of code let me to create different players with one Bingo Card for once.
+// const ingameBingoCard = {};
+
+// for (let i = 0; i < 100; i++) { // The condition let me to control the number of players.
+//     ingameBingoCard[`Player${i + 1}`] = createBingoCard(); // This function create a Bingo Card object as value of the Players key.
+// }
+
+// // console.log(ingameBingoCard);
+
+// const disBingoCard = []; // A list to collect all the bingo cards in game.
+
+// for (const key in ingameBingoCard) {
+//     disBingoCard.push(`${key}\n${displayBingoCard(ingameBingoCard[key])}`); // This function return the Bingo Card as styled strings.
+// }
+
+// console.log(`Ingame Bingo Cards:\n${disBingoCard.join("\n")}`);
+
+// // The main part of program that simulate the flow of the game
+// const extractedNumbers = []; // Here I collect the extracted number, which will be used inside the markBingoCard().
+
+// for(let i = 0; i < 75; i++) { // In the game there are 75 number extraction max.
+//     const randomNumber = Math.floor(Math.random() * 75 + 1); // Number goes from 1 to 75.
+
+//     if (!extractedNumbers.includes(randomNumber)) { // Extract the number and avoiding duplicates.
+//         extractedNumbers.push(randomNumber);
+
+//         for (const key in ingameBingoCard) {
+//             markBingoCard(ingameBingoCard[key]); // By refernce, the function mark the extracted number on the Bingo Cards, if there is.
+//         }
+
+//         if (extractedNumbers.length >= 5) { // Only when at least 5 numbers are extracted, we check if one or more player won.
+//             const winningBingoCards = []; // A list to collect the winning Bungo Cards and the name of the player that own it.
+
+//             for (const player in ingameBingoCard) {
+//                 const winningBingoCard = (checkWinningBingoCard(ingameBingoCard[player])); // The function return a Boolean value. In this way we check after every additional extraction. Also, as side effect, the function display which of the 3 winning possibilities happened.
+
+//                 if(winningBingoCard) {
+//                     winningBingoCards.push(`${player}\n${displayBingoCard(ingameBingoCard[player])}`); // Add to the list the name of the player who won the game and his string Bingo Card.
+//                 }
+//             }
+
+//             if (winningBingoCards.length !== 0) { // Only if the list is empty, we can continue to extract numbers.
+//                 console.log(`The winning cards are:\n${winningBingoCards.join("\n")}`); // Display the list the name of the player who won the game and his string Bingo Card.
+
+//                 console.log(`To win we have extracted ${i} numbers.`);
+//                 break; // One or more players won the game, so we can stop.         
+//             }
+            
+//         }
+
+//         continue; // Continue here, allow to follow the normal for loop flow if no duplicated number is extracted.
+//     }
+
+//     i--; // When a duplicate number occours, we discard it and re-do the extraction.
+// }
+
+// console.log(`Extracted number list:\n${extractedNumbers.join("; ")}`);
+
+export {createBingoCard, displayBingoCard, markBingoCard, checkWinningBingoCard};
---
+++ projects/026-play-bingo/js/index.js
@@ -0,0 +1,68 @@
+import {createBingoCard, displayBingoCard, markBingoCard, checkWinningBingoCard} from "./bingo_functions.js";
+
+function randomSort(array) {
+    const elements = array.length;
+    for (let i = elements; i > 0; --i ) {
+        const randomIndex = Math.floor(Math.random() * elements);
+
+        [ array[randomIndex], array[i - 1] ] = [ array[i - 1], array[randomIndex] ]
+    }
+}
+
+// Simulate games
+const games = 1000; // set the games numbers
+const numbersCalls = []; // create a list of numbers calls needed in every game to make a win
+for (let i = 0; i < games; ++i) {
+
+    // create the pool of numbers that can be extracted
+    const poolNumbers = [];
+    const endNumber = 75;
+    for (let i = 1; i <= endNumber; ++i) {
+    
+    if (i <= 15)
+    poolNumbers.push("B" + i);
+    else if (i <= 30)
+    poolNumbers.push("I" + i);
+    else if (i <= 45)
+    poolNumbers.push("N" + i);
+    else if (i <= 60)
+    poolNumbers.push("G" + i);
+    else poolNumbers.push("O" + i);
+    }
+
+    // Sorting the numbers to prepare the game
+    randomSort(poolNumbers);
+
+    // Init a player with a BingoCard
+    const player1 = createBingoCard();
+    // displayBingoCard(player1);
+
+    // Core of the game
+    const totalNumbers = poolNumbers.length;
+    while (totalNumbers !== 0){ // Game proceed till all the numbers are extracted
+
+        const extractNumber = Number.parseInt(poolNumbers.pop().substring(1), 10); // Extract a sorted number from the tale of the list
+
+        markBingoCard(player1, extractNumber); // Replace the number in the card with a 0, if there is a match
+
+        // Check if the card is winnning
+        if (checkWinningBingoCard(player1)) { // The function as side effect give the type of winning (column, row or diagonal)
+
+            numbersCalls.push(endNumber - poolNumbers.length); // Computing how many numbers calls had done to make a win and push it to the list
+
+            // console.log(`Winning card:`);
+            // displayBingoCard(player1);
+            break; // If the player won, we can exit the game
+        }
+    }
+}
+
+// Statistics of the games
+numbersCalls.sort( (a, b) => a - b); // sort the list in ascending order
+
+let averageCalls = 0;
+numbersCalls.forEach( number => averageCalls += number);
+
+console.log(`Average calls for ${games} games: ${averageCalls / games}\n`); // calculate the average of the calls
+console.log(`Minimum calls for ${games} games: ${numbersCalls.shift()}\n`); // remove and return the first element of the list, as he correspond to the minimum
+console.log(`Maximum calls for ${games} games: ${numbersCalls.pop()}\n`); // remove and return the last element of the list, as he correspond to the maximum
