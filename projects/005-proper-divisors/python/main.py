110ca2baf2112f1f466cce26eb1f8bad5670c8f2
Noeddasheep <muceteo@gmail.com>
2023-12-16 18:12:04 +0000

    solution proper_divisors

---
+++ projects/005-proper-divisors/python/main.py
@@ -0,0 +1,22 @@
+def proper_divisor(integer):
+    divisors = []
+    i = 1
+
+    while i <= integer:
+
+        if integer % i == 0:
+            divisors.append(i)
+
+        i += 1
+
+    return divisors
+
+
+def main():
+    user_integer = int(input("Enter an integer:\n"))
+
+    print(f"The divisors of {user_integer} are: {proper_divisor(user_integer)}")
+
+
+if __name__ == '__main__':
+    main()
