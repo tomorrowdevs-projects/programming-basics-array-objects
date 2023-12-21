f72371c9363533207e9c889ce6cd12e1e953a275
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-12-21 14:44:12 +0100

    added solution

--- projects/001-sorted-order/js/index.js
+++ projects/001-sorted-order/js/index.js
@@ -1,26 +1,24 @@
 'use strict';
-let numbers = [];
+let integers = [];
 
-let insertedNumber = true;
+let insertedInteger = true;
 
-while (insertedNumber) {
+while (insertedInteger) {
     let inputNumber = prompt('Enter a integer or digit 0 to go out');
 
     if (inputNumber === '0') {
-        insertedNumber = false;
+        insertedInteger = false;
     } else {
-        let num = parseInt(inputNumber, 10);
+        let num = parseInt(inputNumber, 10); // To convert the string in a number with base 10
 
         if (!isNaN(num)) {
-            numbers.push(num);
+            integers.push(num);
         } else {
             console.log('Enter a valid integer');
         }
     }
 }
 
-console.log(numbers);
-let arr = numbers.sort(function (a, b) {
-    return a - b;
-});
-console.log(arr);
+console.log(integers);
+integers.reverse();
+console.log(integers);
--- projects/002-reverse-order/js/index.js
+++ projects/002-reverse-order/js/index.js
@@ -1,1 +1,25 @@
 'use strict';
+
+let numbers = [];
+
+let insertedNumber = true;
+
+while (insertedNumber) {
+    let inputNumber = prompt('Enter a integer or digit 0 to go out');
+
+    if (inputNumber === '0') {
+        insertedNumber = false;
+    } else {
+        let num = parseInt(inputNumber, 10);
+
+        if (!isNaN(num)) {
+            numbers.push(num);
+        } else {
+            console.log('Enter a valid integer');
+        }
+    }
+}
+
+console.log(numbers);
+let arr = numbers.reverse();
+console.log(arr);
