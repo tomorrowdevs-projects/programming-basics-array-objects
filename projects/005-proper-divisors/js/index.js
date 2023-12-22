03ea532fd95a54e3980b983a0d1e0cef58ebd400
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-12-22 18:32:11 +0100

    Added solution of the exercise

--- projects/005-proper-divisors/js/index.js
+++ projects/005-proper-divisors/js/index.js
@@ -1,1 +1,31 @@
 'use strict';
+
+function getProperDivisors(positivNumber) {
+    for (let num = 1; num <= positivNumber; num++) {
+        if (
+            positivNumber % num === 0 &&
+            !isNaN(positivNumber) &&
+            positivNumber !== '' &&
+            positivNumber !== ' '
+        ) {
+            properDivisor.push(num);
+        }
+    }
+    return properDivisor;
+}
+
+const positivNumber = Number(
+    prompt(
+        'Enter a positive number for which you want to find its correct divisors.'
+    )
+);
+
+const properDivisor = [];
+
+const result = getProperDivisors(positivNumber);
+
+if (isNaN(positivNumber)) {
+    console.log('Enter a valid number');
+} else {
+    console.log(result);
+}
