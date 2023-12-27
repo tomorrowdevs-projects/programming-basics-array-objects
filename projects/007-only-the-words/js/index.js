070ed813ffa76dc067bf9d16782cf05b5bdb8899
AlessandroVerzella <116301354+AlessandroVerzella@users.noreply.github.com>
2023-12-27 19:54:05 +0100

    Entered solution and comments

--- projects/007-only-the-words/js/index.js
+++ projects/007-only-the-words/js/index.js
@@ -1,1 +1,28 @@
 'use strict';
+
+function removePunctuationMarks(str) {
+    if (
+        str.includes(',') ||
+        str.includes('-') ||
+        str.includes('.') ||
+        str.includes('?') ||
+        str.includes('-') ||
+        str.includes(" '") ||
+        str.includes("' ") ||
+        str.includes("'''") ||
+        str.includes('!') ||
+        str.includes(':') ||
+        str.includes(';')
+    ) {
+        let arr = str.replace(/[,-.?!:;']/g, '').split(' ');
+        return arr;
+    } else {
+        let arr = str.split(' '); // If there are no punctuation marks indicated in the text of the exercise
+        return arr;
+    }
+}
+
+// main programm
+const str = prompt('Enter all the words that you want');
+const checkPunctuationMarks = removePunctuationMarks(str);
+console.log(checkPunctuationMarks);
