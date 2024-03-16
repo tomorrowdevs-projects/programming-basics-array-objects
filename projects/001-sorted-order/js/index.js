18a9bb9aecef15a5b3c53aa74b9d8a2a31c70d49
AlessandroVerzella <alessandro.verzella@web.de>
2024-03-16 14:03:42 +0100

    Added file Html

--- projects/001-sorted-order/js/index.js
+++ projects/001-sorted-order/js/index.js
@@ -1,1 +1,24 @@
 'use strict';
+let integers = [];
+
+let insertedInteger = true;
+
+while (insertedInteger) {
+    let inputNumber = prompt('Enter a integer or digit 0 to go out');
+
+    if (inputNumber === '0') {
+        insertedInteger = false;
+    } else {
+        let num = parseInt(inputNumber, 10); // To convert the string in a number with base 10
+
+        if (!isNaN(num)) {
+            integers.push(num);
+        } else {
+            console.log('Enter a valid integer');
+        }
+    }
+}
+
+console.log(integers);
+integers.reverse();
+console.log(integers);
