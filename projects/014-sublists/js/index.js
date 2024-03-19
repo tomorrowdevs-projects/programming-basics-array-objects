638e28a4a64348906c3ba54ad26c71d395ff678f
AlessandroVerzella <alessandro.verzella@web.de>
2024-03-19 17:57:01 +0100

    Added solution of the exercise and some comments

--- projects/014-sublists/js/index.js
+++ projects/014-sublists/js/index.js
@@ -1,1 +1,44 @@
 'use strict';
+
+function isSublist(firstList, secondList) {
+    for (
+        let i = 0, j = 0;
+        i < firstList.length, j < secondList.length;
+        i++, j++
+    ) {
+        // Checking if one of the 2 arrays is empty
+        if (
+            secondList.includes(' ') ||
+            secondList.includes('') ||
+            firstList.includes(' ') ||
+            firstList.includes('')
+        ) {
+            return true;
+            break;
+        } else if (firstList.length >= secondList.length) {
+            // Loop in the second array to check it is a Sublist
+            if (firstList.indexOf(secondList[j]) === i) {
+                return true;
+            } else {
+                return false;
+                break;
+            }
+        } else if (firstList.length < secondList.length) {
+            // Loop in the first array to check it is a Sublist
+            if (secondList.indexOf(firstList[i]) === j) {
+                return true;
+            } else {
+                return false;
+                break;
+            }
+        }
+    }
+}
+// Main program
+const str = prompt('Digit something');
+const str2 = prompt('Digit something else');
+const firstList = str.split(' ');
+const secondList = str2.split(' ');
+
+const theListIsSubList = isSublist(firstList, secondList);
+console.log(theListIsSubList);
