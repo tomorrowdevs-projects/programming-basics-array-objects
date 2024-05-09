486136733127ac6e1c4a2436fc9caf5c1a2ba808
AlessandroVerzella <alessandro.verzella@web.de>
2024-05-09 19:44:05 +0200

    Added some modification to the exercise

--- projects/025-checking-for-a-winning-card/js/index.js
+++ projects/025-checking-for-a-winning-card/js/index.js
@@ -1,102 +1,61 @@
 'use strict';
-function randomBingoCard() {
-    let letterB = Array.from({ length: 5 }, () =>
-        Math.floor(Math.random() * 15 + 1)
-    ); // From method to obtain an array of tandom numbers
 
-    let emptyArrLetterB = [];
+function randomNumber(min, max) {
+    let randomNum = new Set();
 
-    while (emptyArrLetterB.length < 5) {
-        letterB = Math.floor(Math.random() * 15 + 1);
-        if (emptyArrLetterB.indexOf(letterB) === -1) {
-            emptyArrLetterB.push(letterB);
-        } // To eliminate duplicates and replace them with different numbers
+    while (randomNum.size < 5) {
+        randomNum.add(Math.floor(Math.random() * (max - min) + min));
     }
 
-    let letterI = Array.from({ length: 5 }, () =>
-        Math.floor(Math.random() * (31 - 16) + 16)
-    );
-    let emptyArrLetterI = [];
-    while (emptyArrLetterI.length < 5) {
-        letterI = Math.floor(Math.random() * (31 - 16) + 16);
-        if (emptyArrLetterI.indexOf(letterI) === -1) {
-            emptyArrLetterI.push(letterI);
-        }
-    }
-    let letterN = Array.from({ length: 5 }, () =>
-        Math.floor(Math.random() * (46 - 31) + 31)
-    );
-    let emptyArrLetterN = [];
-    while (emptyArrLetterN.length < 5) {
-        letterN = Math.floor(Math.random() * (46 - 31) + 31);
-        if (emptyArrLetterN.indexOf(letterN) === -1) {
-            emptyArrLetterN.push(letterN);
-        }
-    }
-    let letterG = Array.from({ length: 5 }, () =>
-        Math.floor(Math.random() * (61 - 46) + 46)
-    );
-    let emptyArrLetterG = [];
-    while (emptyArrLetterG.length < 5) {
-        letterG = Math.floor(Math.random() * (61 - 46) + 46);
-        if (emptyArrLetterG.indexOf(letterG) === -1) {
-            emptyArrLetterG.push(letterG);
-        }
-    }
-    let letterO = Array.from({ length: 5 }, () =>
-        Math.floor(Math.random() * (76 - 61) + 61)
-    );
-    let emptyArrLetterO = [];
-    while (emptyArrLetterO.length < 5) {
-        letterO = Math.floor(Math.random() * (76 - 61) + 61);
-        if (emptyArrLetterO.indexOf(letterO) === -1) {
-            emptyArrLetterO.push(letterO);
-        }
-    }
-    // Sort to put all numbers in the columns of the bingo card  in ascending order
-    emptyArrLetterB.sort(function (a, b) {
-        return a - b;
-    });
-    emptyArrLetterI.sort(function (a, b) {
-        return a - b;
-    });
-    emptyArrLetterN.sort(function (a, b) {
-        return a - b;
-    });
-    emptyArrLetterG.sort(function (a, b) {
-        return a - b;
-    });
-    emptyArrLetterO.sort(function (a, b) {
-        return a - b;
-    });
-
-    return {
-        emptyArrLetterB,
-        emptyArrLetterI,
-        emptyArrLetterN,
-        emptyArrLetterG,
-        emptyArrLetterO,
-    };
+    return [...randomNum].sort((a, b) => a - b);
 }
 
-// The function accepts the Bingo card and the number called as arguments.
-function getCalledNumber(card, number) {
+const generateBingoCard = () => {
+    const bingostring = 'BINGO';
+    let bingoCard = [...bingostring];
+    let min = 1;
+    let max = 16;
+    for (let i = 0; i < 5; i++) {
+        const bingoarray = randomNumber(min, max);
+        bingoCard[i] = [bingoCard[i], bingoarray];
+        min = max;
+        max = max + 15;
+    }
+    return bingoCard;
+};
+
+const printBingoCard = (card) => {
+    const cardtostamp = card.map((row) => [row[0], ...row[1]]);
+    const cardrows = cardtostamp[0].length;
+    const cardcols = cardtostamp.length;
+    for (let i = 0; i < cardrows; i++) {
+        let linetoprint = '';
+        for (let j = 0; j < cardcols; j++) {
+            const element = cardtostamp[j][i];
+
+            linetoprint += element.toString().padStart(5, ' ');
+        }
+        console.log(linetoprint);
+    }
+};
+
+const replaceNumber = (card, number) => {
     for (let key in card) {
-        // Search the called numbers in each column of the bingo card ane replace with 0 if present
-        if (card[key].includes(number)) {
-            card[key][card[key].indexOf(number)] = 0;
+        if (card[key][1].includes(number)) {
+            const index = card[key][1].indexOf(number);
+            card[key][1][index] = 0;
         }
     }
+};
+
+const bcard = generateBingoCard();
+printBingoCard(bcard);
+
+const calledNumber = new Set();
+for (let m = 1; m <= 75; m++) {
+    let num = Math.floor(Math.random() * 75 + 1);
+    calledNumber.add(num);
+    replaceNumber(bcard, num);
 }
 
-let result = {};
-result = randomBingoCard();
-
-const calledNumbers = new Set();
-for (let j = 1; j <= 75; j++) {
-    let randomNumber = Math.floor(Math.random() * 75 + 1);
-    calledNumbers.add(randomNumber);
-    getCalledNumber(result, randomNumber);
-}
-
-console.log(result);
+printBingoCard(bcard);
