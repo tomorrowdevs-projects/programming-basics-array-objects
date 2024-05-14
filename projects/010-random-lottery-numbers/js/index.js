d8c61d05e7e3ea191763262f96f1768484bfcc24
AlessandroVerzella <alessandro.verzella@web.de>
2024-05-14 21:49:06 +0200

    Entered a new solution of the exercise and new comments

--- projects/010-random-lottery-numbers/js/index.js
+++ projects/010-random-lottery-numbers/js/index.js
@@ -1,52 +1,37 @@
 'use strict';
 
-let numbersLottery = [];
+// Function to calculate the minimum and maximum of certain random numbers
+const randomLotteryNumbers = (min, max) => {
+    const randomNumber = new Set();
 
-while (numbersLottery.length < 6) {
-    if (true) {
-        let randomNumber = Math.trunc(Math.random() * 49 + 1);
-        numbersLottery.push(randomNumber);
-    } else {
-        break;
+    while (randomNumber.size < 6) {
+        randomNumber.add(Math.floor(Math.random() * (max - min) + min));
     }
-}
+    return [...randomNumber].sort((a, b) => a - b);
+};
 
-console.log(numbersLottery);
+const ticketLotteryNumbers = (min, max) => {
+    const randomNum = new Set();
 
-if (
-    numbersLottery[0] !== numbersLottery[1] &&
-    numbersLottery[0] !== numbersLottery[2] &&
-    numbersLottery[0] !== numbersLottery[3] &&
-    numbersLottery[0] !== numbersLottery[4] &&
-    numbersLottery[0] !== numbersLottery[5] &&
-    numbersLottery[1] !== numbersLottery[0] &&
-    numbersLottery[1] !== numbersLottery[2] &&
-    numbersLottery[1] !== numbersLottery[3] &&
-    numbersLottery[1] !== numbersLottery[4] &&
-    numbersLottery[1] !== numbersLottery[5] &&
-    numbersLottery[2] !== numbersLottery[0] &&
-    numbersLottery[2] !== numbersLottery[1] &&
-    numbersLottery[2] !== numbersLottery[3] &&
-    numbersLottery[2] !== numbersLottery[4] &&
-    numbersLottery[2] !== numbersLottery[5] &&
-    numbersLottery[3] !== numbersLottery[0] &&
-    numbersLottery[3] !== numbersLottery[1] &&
-    numbersLottery[3] !== numbersLottery[2] &&
-    numbersLottery[3] !== numbersLottery[4] &&
-    numbersLottery[3] !== numbersLottery[5] &&
-    numbersLottery[4] !== numbersLottery[0] &&
-    numbersLottery[4] !== numbersLottery[1] &&
-    numbersLottery[4] !== numbersLottery[2] &&
-    numbersLottery[4] !== numbersLottery[3] &&
-    numbersLottery[4] !== numbersLottery[5] &&
-    numbersLottery[5] !== numbersLottery[0] &&
-    numbersLottery[5] !== numbersLottery[1] &&
-    numbersLottery[5] !== numbersLottery[2] &&
-    numbersLottery[5] !== numbersLottery[3] &&
-    numbersLottery[5] !== numbersLottery[4]
-) {
-    const sortednumbersLottery = numbersLottery.sort((a, b) => a - b);
-    console.log(sortednumbersLottery);
-} else {
-    console.log('Operation not valid');
-}
+    while (randomNum.size < 6) {
+        randomNum.add(Math.floor(Math.random() * (max - min) + min));
+    }
+    return [...randomNum].sort((a, b) => a - b);
+};
+
+const lotteryNumbers = randomLotteryNumbers(1, 49);
+const ticketNumbers = ticketLotteryNumbers(1, 49);
+
+// Comparing the numbers of the two arrays
+const compareTickets = (lotteryNumbers, ticketNumbers) => {
+    return JSON.stringify(lotteryNumbers) === JSON.stringify(ticketNumbers);
+};
+
+// Ternary operator to check whether the ticket is a winning one
+const checkTickets = compareTickets(lotteryNumbers, ticketNumbers)
+    ? console.log(
+          `Your ticket numbers are ${lotteryNumbers}; while the numbers drawn in the lottery are: ${ticketNumbers}. Therefore your ticket is a winner.`
+      )
+    : console.log(
+          `Your ticket numbers are ${lotteryNumbers}; while the numbers drawn in the lottery are: ${ticketNumbers}. Therefore your ticket is not a winner.`
+      );
