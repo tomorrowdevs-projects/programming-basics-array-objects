27ba4fab01273f01196bdee2ebab3f61d10362d6
AlessandroVerzella <alessandro.verzella@web.de>
2024-05-15 18:31:17 +0200

    Removed the second function in order to comply with the Don't repeat yourself principle.

--- projects/010-random-lottery-numbers/js/index.js
+++ projects/010-random-lottery-numbers/js/index.js
@@ -10,17 +10,8 @@ const randomLotteryNumbers = (min, max) => {
     return [...randomNumber].sort((a, b) => a - b);
 };
 
-const ticketLotteryNumbers = (min, max) => {
-    const randomNum = new Set();
-
-    while (randomNum.size < 6) {
-        randomNum.add(Math.floor(Math.random() * (max - min) + min));
-    }
-    return [...randomNum].sort((a, b) => a - b);
-};
-
 const lotteryNumbers = randomLotteryNumbers(1, 49);
-const ticketNumbers = ticketLotteryNumbers(1, 49);
+const ticketNumbers = randomLotteryNumbers(1, 49);
 
 // Comparing the numbers of the two arrays
 const compareTickets = (lotteryNumbers, ticketNumbers) => {
@@ -30,8 +21,12 @@ const compareTickets = (lotteryNumbers, ticketNumbers) => {
 // Ternary operator to check whether the ticket is a winning one
 const checkTickets = compareTickets(lotteryNumbers, ticketNumbers)
     ? console.log(
-          `Your ticket numbers are ${lotteryNumbers}; while the numbers drawn in the lottery are: ${ticketNumbers}. Therefore your ticket is a winner.`
+          `Your ticket numbers are ${lotteryNumbers}; 
+          The numbers drawn in the lottery are: ${ticketNumbers}. 
+          Therefore your ticket is a winner.`
       )
     : console.log(
-          `Your ticket numbers are ${lotteryNumbers}; while the numbers drawn in the lottery are: ${ticketNumbers}. Therefore your ticket is not a winner.`
+          `Your ticket numbers are ${lotteryNumbers}; 
+          The numbers drawn in the lottery are: ${ticketNumbers}. 
+          Therefore your ticket is not a winner.`
       );
