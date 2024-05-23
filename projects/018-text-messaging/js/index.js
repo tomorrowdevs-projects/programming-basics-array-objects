ed4b7e490b9ca95c8e9e63e126aa743b5f65094e
AlessandroVerzella <alessandro.verzella@web.de>
2024-05-23 21:08:20 +0200

    Added exercise as requested. However, I could not avoid the automatic sorting of the Object numeric property

--- projects/018-text-messaging/js/index.js
+++ projects/018-text-messaging/js/index.js
@@ -1,83 +1,50 @@
 'use strict';
-function getTextMessaging(arrayUserMessage) {
-    const arr = [];
-    for (let i = 0; i < arrayUserMessage.length; i++) {
-        arr.push(symbolsKeyPad[arrayUserMessage[i]]); // IPush all each elements of the array into the empty array in combination with the Keys of the object
-    }
 
-    const keyPresses = arr.toString().replaceAll(',', ''); // To replace the commas I obtain by converting the array into a string
-    return keyPresses;
-}
+const arrayUserMessage = [
+    ...prompt(
+        "Type a message to see how many times you have to press the key on your phone's keyboard to type the message text"
+    ),
+];
+console.log(arrayUserMessage);
+// console.log(typeof arrayUserMessage);
 
-// Main programm
-
-const userMessage = prompt(
-    "Type a message to see how many times you have to press the key on your phone's keyboard to type the message text"
-);
-const arrayUserMessage = userMessage.split(''); //  Convert the string in an array
-
-const symbolsKeyPad = {
-    '.': 1,
-    ',': 11,
-    '?': 111,
-    '!': 1111,
-    ':': 11111,
-    A: 2,
-    a: 2,
-    B: 22,
-    b: 22,
-    C: 222,
-    c: 222,
-    D: 3,
-    d: 3,
-    E: 33,
-    e: 33,
-    F: 333,
-    f: 333,
-    G: 4,
-    g: 4,
-    H: 44,
-    h: 44,
-    I: 444,
-    i: 444,
-    J: 5,
-    j: 5,
-    K: 55,
-    k: 55,
-    L: 555,
-    l: 555,
-    M: 6,
-    m: 6,
-    N: 66,
-    n: 66,
-    O: 666,
-    o: 666,
-    P: 7,
-    p: 7,
-    Q: 77,
-    q: 77,
-    R: 777,
-    r: 777,
-    S: 7777,
-    s: 7777,
-    T: 8,
-    t: 8,
-    U: 88,
-    u: 88,
-    V: 888,
-    v: 888,
-    W: 9,
-    w: 9,
-    X: 99,
-    x: 99,
-    Y: 999,
-    y: 999,
-    Z: 9999,
-    z: 9999,
-    ' ': 0,
+const obj = {
+    1: ['.', ',', '?', '!', ':'],
+    2: ['A', 'a', 'B', 'b', 'C', 'c'],
+    3: ['D', 'd', 'E', 'e', 'F', 'f'],
+    4: ['G', 'g', 'H', 'h', 'I', 'i'],
+    5: ['J', 'j', 'K', 'k', 'L', 'l'],
+    6: ['M', 'm', 'N', 'n', 'O', 'o'],
+    7: ['P', 'p', 'Q', 'q', 'R', 'r', 'S', 's'],
+    8: ['T', 't', 'U', 'u', 'V', 'v'],
+    9: ['W', 'w', 'X', 'x', 'Y', 'y', 'Z', 'z'],
+    0: [' '],
 };
 
-const keypressesRequired = getTextMessaging(arrayUserMessage);
-console.log(
-    `For the user's input message "${userMessage}", the following key presses are required on the phone keypad: ${keypressesRequired} `
-);
+const emptyArr = [];
+
+for (const [key, value] of Object.entries(obj)) {
+    for (let i = 0; i < arrayUserMessage.length; i++) {
+        const index = value.indexOf(arrayUserMessage[i]);
+        if (value.includes(arrayUserMessage[[i]])) {
+            `${[key.repeat(index)].toString()}`;
+            if (
+                [key.repeat(index)].toString() >= 22 &&
+                [key.repeat(index)].toString() <= 99999999
+            ) {
+                console.log(`${[key.repeat(Math.ceil(index / 2))].toString()}`);
+            } else if (
+                [key.repeat(index)].toString() >= 2 &&
+                [key.repeat(index)].toString() <= 9
+            ) {
+                console.log(`${[key.repeat(index)].toString()}`);
+            } else {
+                console.log(`${[key.repeat(index)].toString()}1`);
+            }
+
+            emptyArr.push(key.repeat(index));
+        }
+    }
+}
+const digitMessage = emptyArr.toString().replaceAll(',', '');
+console.log(digitMessage);
