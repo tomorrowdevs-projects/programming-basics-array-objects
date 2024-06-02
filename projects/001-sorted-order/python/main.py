85b18a27f3c0c29d81610c39459ab44fd6f1c1ea
Roberto <robertomoggia95@gmail.com>
2024-06-03 00:11:27 +0200

    solution/m1-001
    First commit

---
+++ projects/001-sorted-order/python/main.py
@@ -0,0 +1,11 @@
+number=input('Please enter an integer:')
+number_list=list()
+while number!='0':
+    if number.isdecimal():
+        number_list.append(number)
+        number=input('Please enter an integer or enter 0 to print all the previous values:')
+    else:
+        number=input('You have entered a wrong value, please enter an integer or enter 0 to print all the previous values:')
+
+for number in number_list:
+    print(number)
