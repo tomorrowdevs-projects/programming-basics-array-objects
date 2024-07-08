a4015c75b1976615f9d192b666a9f396898fd87e
BloodYce <mattyago99@gmail.com>
2024-07-08 22:45:32 +0200

    Undicesimo Esercizio

---
+++ projects/011-pig-latin/python/main.py
@@ -0,0 +1,22 @@
+'''
+Pig Latin is a language constructed by transforming English words. While the ori- gins of the language are unknown, it is mentioned in at least two documents from the nineteenth century, suggesting that it has existed for more than 100 years. The
+following rules are used to translate English into Pig Latin:
+
+- If the word begins with a consonant (includingy), then all letters at the beginning of the word, up to the first vowel (excluding y), are removed and then added to the end of the word, followed by ay. For example, *computer* becomes *omputercay* and *think* becomes *inkthay*.
+
+- If the word begins with a vowel (not including y), then way is added to the end of the word. For example, algorithm becomes *algorithmway* and office becomes *officeway*.
+
+Write a program that reads a line of text from the user. 
+Then your program should translate the line into Pig Latin and display the result. 
+You may assume that the string entered by the user only contains lowercase letters and spaces.
+'''
+
+normalword = str(input("Please input a word: "))
+consonants = '''bcdfghjklmnpqrstvwxz'''
+
+if normalword[0] == "y" or normalword[0] in consonants:
+    piglatinword = normalword[1:] + normalword[0] + "ay"
+else:
+    piglatinword = normalword + "way"
+
+print(piglatinword)
