b7cff2ab3eb1de0928a670dff98d698f65866812
Stefano Agnelli <stefano.agnelli.jobs@gmail.com>
2024-12-27 15:32:17 +0100

    solution m3-022-optimized

--- projects/022-anagrams/python/main.py
+++ projects/022-anagrams/python/main.py
@@ -1,19 +1,26 @@
-def hist(string):
-    word_dict = dict()
-    for char in string:
-        if char in word_dict:
-            word_dict[char] += 1
-        
-        else:
-            word_dict[char] = 1
-    
-    return word_dict
+def hist(string_1, string_2 ):
 
+    if len(string_1) != len(string_2):
+        return False
+    
+    word_dict_1 = list()
+    word_dict_2 = list()  
+    
+    for i in range(len(string_1)):
+        word_dict_1.append(string_1[i])
+        word_dict_2.append(string_2[i])
+       
+    word_dict_1.sort()
+    word_dict_2.sort()
+
+    return word_dict_1 == word_dict_2
 
 user_string_1 = input('insert first word: ').lower()
 user_string_2 = input('insert second word: ').lower()
 
-if hist(user_string_1) == hist(user_string_2):
+is_anagram = hist(user_string_1, user_string_2)
+
+if is_anagram:
     print(f'{user_string_1} and {user_string_2} are anagrams.')
 
 else:
